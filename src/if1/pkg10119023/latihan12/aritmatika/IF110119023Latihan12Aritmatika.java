798748016ca6412e1f542e738b54c14ce7b09df7
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 20:34:51 +0700

    Latihan 12 - Aritmatika

---
+++ src/if1/pkg10119023/latihan12/aritmatika/IF110119023Latihan12Aritmatika.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : latihan tanda operator aritmatika
+ */
+public class IF110119023Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("b--   = " + (b--));
+        System.out.println("d++   = " + (d++));
+        System.out.println("++d   = " + (++d));
+        System.out.println(a++); //nyoba
+        System.out.println(a);
+    }
+    
+}
